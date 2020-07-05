972a6e78d3a5e2a4f010c0fe2727965ac4623c61
Lilyliod <kurosabarai11@gmail.com>
2020-07-05 05:52:26 +0100

    auth with google (alone)

---
+++ app/src/main/java/com/example/authentification/MainActivity.kt
@@ -0,0 +1,103 @@
+package com.example.authentification
+
+import android.accessibilityservice.GestureDescription
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var mGoogleSignInClient: GoogleSignInClient
+    private val RC_SIGN_IN = 9001
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken("319806624886-c2ftjuhv969m9kqfmegm2ei1krd7v09f.apps.googleusercontent.com")
+                .requestEmail()
+                .build()
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        google_login_btn.setOnClickListener {
+            signIn()
+        }
+
+        google_logout_btn.setOnClickListener{
+            signOut()
+        }
+
+    }
+
+    private fun signIn() {
+        val signInIntent = mGoogleSignInClient.signInIntent
+        startActivityForResult(
+                signInIntent, RC_SIGN_IN
+        )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            val task =
+                    GoogleSignIn.getSignedInAccountFromIntent(data)
+                    handleSignInResult(task)
+        }
+    }
+
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(
+                    ApiException::class.java
+            )
+            // Signed in successfully
+            val googleId = account?.id ?: ""
+            Log.i("Google ID",googleId)
+
+            val googleFirstName = account?.givenName ?: ""
+            Log.i("Google First Name", googleFirstName)
+
+            val googleLastName = account?.familyName ?: ""
+            Log.i("Google Last Name", googleLastName)
+
+            val googleEmail = account?.email ?: ""
+            Log.i("Google Email", googleEmail)
+
+            val googleProfilePicURL = account?.photoUrl.toString()
+            Log.i("Google Profile Pic URL", googleProfilePicURL)
+
+            val googleIdToken = account?.idToken ?: ""
+            Log.i("Google ID Token", googleIdToken)
+
+        } catch (e: ApiException) {
+            // Sign in was unsuccessful
+            Log.e(
+                    "failed code=", e.statusCode.toString()
+            )
+        }
+    }
+
+    private fun signOut() {
+        mGoogleSignInClient.signOut()
+                .addOnCompleteListener(this) {
+                    // Update your UI here
+                }
+    }
+
+    private fun revokeAccess() { // delete account
+        mGoogleSignInClient.revokeAccess()
+                .addOnCompleteListener(this) {
+                    // Update your UI here
+                }
+    }
+}
